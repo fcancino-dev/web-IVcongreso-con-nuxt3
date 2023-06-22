ee02b8eea65b30f9ff3fe4103f656eec0f556218
francoD-fr <franco.cancino@unapiquitos.edu.pe>
2023-06-22 15:49:06 -0500

    add background to the ponentes section

--- components/landing/components/sections/ponentesSection.vue
+++ components/landing/components/sections/ponentesSection.vue
@@ -1,4 +1,5 @@
 <script setup>
+import speaker1 from '@/assets/images/speaker-fondo.png';
 
 const speakers = [
     {
@@ -54,29 +55,34 @@ const speakers = [
 </script>
 
 <template>
-    <section class=" w-full bg-gray-200 pt-24 pb-[56px]">
-        <div class="fr-container">
-            <div class="space-y-10">
-                <div class=" space-y-2">
-                    <h1 class="text-4xl font-bold font-[exo 2] leading-[60px] flex justify-center">Ponentes Principales del IV Congreso</h1>
-                    <p class="text-[20px] leading-6 text-gray-500 text-center font-[exo 2]"> 
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus deleniti fugit officia officiis,
-                        odio asperiores necessitatibus veritatis sequi dicta consectetur? Pariatur cum, numquam cupiditate
-                    </p>
-                </div>
-                <div class="flex flex-wrap justify-center">
-                    <GeneralCard
-                        class="pr-[22.5px] pb-10" 
-                        v-for="speaker in speakers " 
-                        :key="speaker.id"
-                        :speaker="speaker.name"
-                        :role="speaker.role"
-                        :companyName="speaker.companyName"
-                    />
+    <section class=" w-full bg-gray-200 ">
+        <div class=" h-full bg-cover bg-center bg-no-repeat bg-fixed" 
+            :style="{ backgroundImage:`url(${speaker1})`}"
+        >
+            <div class="fr-container pt-24 pb-[56px]">
+                <div class="space-y-10">
+                    <div class=" space-y-2">
+                        <h1 class="text-4xl font-bold font-[exo 2] leading-[60px] flex justify-center">Ponentes Principales del IV Congreso</h1>
+                        <p class="text-[20px] leading-6 text-gray-500 text-center font-[exo 2]"> 
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus deleniti fugit officia officiis,
+                            odio asperiores necessitatibus veritatis sequi dicta consectetur? Pariatur cum, numquam cupiditate
+                        </p>
+                    </div>
+                    <div class="flex flex-wrap justify-center">
+                        <GeneralCard
+                            class="pr-[22.5px] pb-10" 
+                            v-for="speaker in speakers " 
+                            :key="speaker.id"
+                            :speaker="speaker.name"
+                            :role="speaker.role"
+                            :companyName="speaker.companyName"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
     </section>
 </template>
 
-<style></style>
+<style scoped>
+</style>
--- components/landing/components/sections/queEsSection.vue
+++ components/landing/components/sections/queEsSection.vue
@@ -1,8 +1,8 @@
 <template>
-    <section class=" w-full bg-white py-24">
+    <section class=" w-full bg-[#404b6c] py-24">
         <div class="fr-container">
             <div class=" flex space-x-6 ">
-                <p class="text-center text-4xl font-bold font-[exo 2] leading-[45px]">"Descubre el IV Congreso Regional: Explorando las Aplicaciones de la Inteligencia Artificial en la Amazonía Peruana" </p>
+                <p class="text-center text-4xl font-bold font-[exo 2] leading-[45px] text-white">"Descubre el IV Congreso Regional: Explorando las Aplicaciones de la Inteligencia Artificial en la Amazonía Peruana" </p>
                 <!-- <p>
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae enim sunt asperiores quaerat 
                     perspiciatis, distinctio eos facilis ad, aliquam exercitationem iste in laboriosam. Debitis 
